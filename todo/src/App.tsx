478696c6181608f39f61ee240a1321c2b7f8e7ac
Lukasnvc <lukasnvc@gmail.com>
2023-02-27 17:48:03 +0200

    todoapp-tsx

---
+++ todo/src/App.tsx
@@ -0,0 +1,28 @@
+import InputField from './components/InputField';
+import './App.css';
+import { useState } from 'react';
+import { Todo } from './model';
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault()
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }])
+      setTodo('')
+    }
+    
+    console.log(todos)
+
+  }
+  return (
+    <div className="App">
+      <span className='heading'>Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    </div>
+  );
+}
+
+export default App;
